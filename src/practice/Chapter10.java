7eea63db1df6b53b34a3765e8a8abb141defc70d
masakiyamada <109943822+MasakiYamada119@users.noreply.github.com>
2022-08-23 16:22:37 +0900

    新規javaプロジェクト作成

--- src/practice/Chapter10.java
+++ src/practice/Chapter10.java
@@ -12,7 +12,7 @@ public class Chapter10 {
 		 tasks.add(new Task(LocalDate.of(2021,8,10), "散髪に行く。", false));
 		 tasks.add(new Task(LocalDate.of(2021,11,9), "スクールの課題を解く。", false));
 		 long count = tasks.stream().filter(t -> t.isDone() == false).count();
-                System.out.println("未完了のタスクの個数は"+count);
+                System.out.println("未完了のタスクの個数は" + count);
          tasks.stream()
                 .filter(t -> t.isDone() == false)
                 .sorted((t1, t2) -> t1.getDate().compareTo(t2.getDate()))
